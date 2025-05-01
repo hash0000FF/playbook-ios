486d6a51dc418406ed5837b03f7cefe30976cf65
Your Name <kidliaa+hash0000ff@gmail.com>
2025-05-01 13:30:17 +0800

    enable macOS build

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "Playbook",
     platforms: [
         .iOS(.v13)
+        .macOS(.v12)
     ],
     products: [
         .library(name: "Playbook", targets: ["Playbook"]),
